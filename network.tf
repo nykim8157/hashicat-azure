17c64df7b9782e5bb082b85e5d78828ed0cda73d
nykim8157 <nykim8157@gmail.com>
2022-10-26 07:26:22 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,4 +1,7 @@
 module "network" {
   source  = "app.terraform.io/nykim-training/network/azurerm"
   version = "3.5.0"
-  
+  credentials "app.terraform.io" {
+    # valid user API token:
+    token = "xxxxxx.atlasv1.zzzzzzzzzzzzz"
+  }
