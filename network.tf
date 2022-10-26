a6acfa00e855ffd65cfaa16aaac7b1d5d26fb4d2
nykim8157 <nykim8157@gmail.com>
2022-10-26 07:29:53 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,4 +1,12 @@
 module "network" {
   source  = "app.terraform.io/nykim-training/network/azurerm"
   version = "3.5.0"
+  resource "azurerm_resource_group" "myresourcegroup" {
+  name     = "${var.prefix}-workshop"
+  location = var.location
+
+  tags = {
+    environment = "Production"
+  }
+}
   }
